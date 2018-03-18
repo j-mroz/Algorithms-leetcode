df5e54da6f7c947ed5b54556176c386e6b457174
Jaroslaw Mroz <nalik.nal@gmail.com>
2018-03-18 13:38:41 +0100

    Fix spaces

--- hackerrank/strings/morgan_and_a_string.cpp
+++ hackerrank/strings/morgan_and_a_string.cpp
@@ -67,57 +67,57 @@ tuple<ivec, ivec> build_suffix_array(const string &txt) {
 
 int main() {
 
-	auto n = 0;
-	cin >> n;
+    auto n = 0;
+    cin >> n;
 
-	while (n-- > 0) {
-		string first, second;
-		cin >> first >> second;
+    while (n-- > 0) {
+        string first, second;
+        cin >> first >> second;
 
         // Put inpts into one suffix adray, use z as special separator,
         // Knowing the original position of letter we will be able to
         // determine the lexicographicar order of substring starting at given letter
         // from given string.
-		first.push_back('z');
-		second.push_back('z');
-		auto tuple = build_suffix_array(first + second);
-		auto order = get<1>(tuple);
+        first.push_back('z');
+        second.push_back('z');
+        auto tuple = build_suffix_array(first + second);
+        auto order = get<1>(tuple);
 
-		auto f = 0;
-		auto s = 0;
+        auto f = 0;
+        auto s = 0;
 
-		auto fsize = first.size() - 1;
-		auto ssize = second.size()- 1  ;
+        auto fsize = first.size() - 1;
+        auto ssize = second.size()- 1  ;
 
-		string output;
-		while (f < fsize or s < ssize) {
-			if (f < fsize and s < ssize) {
-				if (first[f] < second[s]) {
+        string output;
+        while (f < fsize or s < ssize) {
+            if (f < fsize and s < ssize) {
+                if (first[f] < second[s]) {
                     // first letter is smaller, take it
-					output.push_back(first[f++]);
-				} else if (first[f] > second[s]) {
+                    output.push_back(first[f++]);
+                } else if (first[f] > second[s]) {
                     // second letter is smaller, take it
-					output.push_back(second[s++]);
-				} else {
+                    output.push_back(second[s++]);
+                } else {
                     // letters are equal, compare their suffix array position
-					if (order[f] < order[fsize + 1 + s]) {
-						output.push_back(first[f++]);
-					} else {
-						output.push_back(second[s++]);
-					}
-				}
-			} else if (f < fsize) {
-				output.push_back(first[f++]);
-			} else if (s < ssize) {
-				output.push_back(second[s++]);
-			} else {
-				assert(0 and "unreachable");
-			}
-		}
-		cout << output;
-		if (n != 0)
-			cout << endl;
-	}
+                    if (order[f] < order[fsize + 1 + s]) {
+                        output.push_back(first[f++]);
+                    } else {
+                        output.push_back(second[s++]);
+                    }
+                }
+            } else if (f < fsize) {
+                output.push_back(first[f++]);
+            } else if (s < ssize) {
+                output.push_back(second[s++]);
+            } else {
+                assert(0 and "unreachable");
+            }
+        }
+        cout << output;
+        if (n != 0)
+            cout << endl;
+    }
 
 
     return 0;
